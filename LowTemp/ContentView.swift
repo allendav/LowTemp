99a8184aec85f9d5424468719e4834e1f380ae97
Allen Snook <allendav@automattic.com>
2021-10-17 07:20:52 -0700

    Initial Commit

---
+++ LowTemp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LowTemp
+//
+//  Created by Allen Snook on 10/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LowTemp/LowTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowTempApp.swift
+//  LowTemp
+//
+//  Created by Allen Snook on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LowTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
